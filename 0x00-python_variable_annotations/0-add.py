76a4007554b6a9b01f54518cb697084d6e806b1d
Grace_ngigi <gracengigi859@gmail.com>
2023-12-10 17:57:16 -0800

    fix documentations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,8 @@ annotated add function that adds two annonated variables and return their sum
 
 
 def add(a: float, b: float) -> float:
+    """
+    a type-annotated function add that takes a float a and a float b as arguments
+    returns their sum as a float
+    """
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """
-type-annotated function concat that takes a string str1 and a string str2 as arguments
+type-annotated function that takes a string str1 and a string str2 as args
 returns a concatenated string
 """
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    takes strings and return a concatenated string
+    """
     return str1 + str2
