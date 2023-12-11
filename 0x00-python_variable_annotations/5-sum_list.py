e86020e5355c04f4b2765a9d76196720fcd91d68
Grace_ngigi <gracengigi859@gmail.com>
2023-12-10 21:15:02 -0800

    sum of a lit

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+type-annotated function sum_list
+takes a list input_list of floats as argument
+returns their sum as a float.
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ return sum as float """
+    return sum( input_list)
